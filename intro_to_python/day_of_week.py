d1c68054892c0c0f99d5368778a32d3aebf929fc
kleelof <kleelof@yahoo.com>
2022-12-01 15:29:17 -0800

    updates;

---
+++ intro_to_python/day_of_week.py
@@ -0,0 +1,11 @@
+"""
+  Day of week challenge
+
+    - https://www.almanac.com/how-find-day-week#:~:text=Take%20the%20last%20two%20digits,digits%20(discard%20any%20remainder).&text=Divide%20the%20sum%20by%207,the%20day%20of%20the%20week!
+    - https://cs.uwaterloo.ca/~alopez-o/math-faq/node73.html
+"""
+
+day = 1
+month = 1
+year = 1970
+
